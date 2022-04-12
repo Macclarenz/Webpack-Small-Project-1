221cf2a37433f90fd4abe9f1bfc60bb15fc7e7a1
Mac clarenz Tamang <macclarenz@gmail.com>
2022-04-12 17:09:18 +0800

    upload via VS Code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
+// scss
 import './style/style.scss'
 
+// js modules
 import './js/nav_event'
 import './js/shorten_url'
-// import './js/shorten_create_elements'
-
-console.log('jhnkjnhello')
-
-const get = async () => {
-    return  true
-}
-
